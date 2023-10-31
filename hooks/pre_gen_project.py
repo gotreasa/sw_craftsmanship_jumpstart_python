ed2a98b3a07ee260e81ca02f1ca2ec05712666a6
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-10-31 20:00:07 +0000

    test: add the valid input checks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,5 +1,5 @@
-import re
-import sys
+# Standard Library
+import re, sys, unittest, os, urllib.request
 
 
 MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
@@ -15,3 +15,35 @@ if not re.match(MODULE_REGEX, kata_name):
 
     # Exit to cancel project
     sys.exit(1)
+
+
+def assertNotEmpty(self, obj, message):
+    self.assertTrue(obj, message)
+
+class TestInputIsValid(unittest.TestCase):
+    def test_cookiecutter_values_defined(self):
+        cookie_parameters=[
+            ["kata_name", "{{cookiecutter.kata_name}}"],
+            ["directory_name", "{{cookiecutter.directory_name}}"],
+            ["github_username", "{{cookiecutter.github_username}}"],
+        ]
+        for parameter in  cookie_parameters:
+            with self.subTest(f"🧪 Check that {parameter[0]} is defined", parameter=parameter):
+                f"""🧪 Check that {parameter[0]} is defined"""
+                assertNotEmpty(self, parameter[1], f"❗️ The parameter {parameter[0]} does not have a value")
+
+    def test_repository_url_returns_missing(self):
+        """🧪 Check that the Github repository is not already set up"""
+        try:
+            response = urllib.request.urlopen("https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.kata_name}}.git")
+            self.assertEqual(response.getcode(), 
+            404, 
+            "❗️ The repository already exists on github.com")
+        except urllib.error.HTTPError as e:
+            self.assertEqual(e.code, 
+            404, 
+            "❗️ There was an error connecting with Github")
+
+print('\n\n⌛️ Suite of valid input tests being run\n')
+unittest.main(verbosity=2)
+print('Completed valid input checks')
