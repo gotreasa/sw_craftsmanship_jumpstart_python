dd68faa924c8c079535e0a2f0f9a6496dfb7c116
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-12 23:27:12 +0000

    test: check that ~/.cookiecutterrc is correctly set up

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -1,5 +1,8 @@
-# Standard Library
-import unittest, os, urllib.request
+#!/usr/bin/env python
+# Standard Libraries only
+import unittest, os, stat
+
+COOKIECUTTER_RC_PATH = os.path.expanduser('~') + '/.cookiecutterrc'
 
 class TestPrerequisites(unittest.TestCase):
     def test_gh_is_installed(self):
@@ -26,6 +29,34 @@ class TestPrerequisites(unittest.TestCase):
         """🧪 Asserting PyCharm is added to the path"""
         return_value=os.system('command -v pycharm')
         self.assertEqual(return_value, 0, "❗️ pycharm needs to be added to the system path")
+        
+    def test_cookiecutterrc_setup(self):
+        """🧪 Asserting that CookieCutter RC file is setup correctly"""
+        self.assertTrue(os.path.isfile(COOKIECUTTER_RC_PATH), "❗️ ~/.cookiecutterrc is missing")
+        st = os.stat(COOKIECUTTER_RC_PATH)
+        self.assertTrue(bool(st.st_mode & stat.S_IRUSR), "❗️ ~/.cookiecutterrc does not have owner read permission. Permission needs to be set to 600.")
+        self.assertTrue(bool(st.st_mode & stat.S_IWUSR), "❗️ ~/.cookiecutterrc does not have owner write permission. Permission needs to be set to 600.")
+        self.assertFalse(bool(st.st_mode & stat.S_IXUSR), "❗️ ~/.cookiecutterrc should not have owner execute permission. Permission needs to be set to 600.")
+        self.assertFalse(bool(st.st_mode & stat.S_IRGRP), "❗️ ~/.cookiecutterrc should not have group read permission. Permission needs to be set to 600.")
+        self.assertFalse(bool(st.st_mode & stat.S_IWGRP), "❗️ ~/.cookiecutterrc should not have group write permission. Permission needs to be set to 600.")
+        self.assertFalse(bool(st.st_mode & stat.S_IXGRP), "❗️ ~/.cookiecutterrc should not have group execute permission. Permission needs to be set to 600.")
+        self.assertFalse(bool(st.st_mode & stat.S_IROTH), "❗️ ~/.cookiecutterrc should not give others read permission. Permission needs to be set to 600.")
+        self.assertFalse(bool(st.st_mode & stat.S_IWOTH), "❗️ ~/.cookiecutterrc should not give others write permission. Permission needs to be set to 600.")
+        self.assertFalse(bool(st.st_mode & stat.S_IXOTH), "❗️ ~/.cookiecutterrc should not give others execute permission. Permission needs to be set to 600.")
+        private_tokens = [
+            '"_pact_broker_token": "',
+            '"_sonar_token": "',
+            '"_snyk_token": "',
+        ]
+        
+        for token in private_tokens:
+            with open(COOKIECUTTER_RC_PATH, "r") as file:
+                found=False
+                for line_number, line in enumerate(file):  
+                    if token in line:
+                        found=True
+                        break
+                self.assertTrue(found, f'❗️ The ~/.cookiecutterrc was not updated with {token}..."')
 
 print('\n\n⌛️ Suite of prerequisite tests being run\n')
 unittest.main(verbosity=2)
